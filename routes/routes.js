0ebc57bb55b78e50a8c84b4a3b2013a7422929d9
MauriBTC <iorimaurizio95@gmail.com>
2024-07-06 17:07:40 +0200

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+// create NFT representing the user CV
+router.get('/user/createcv', (req, res) => {
+    console.log("creating CV")
+    // create a new wallet, obtain lamports, create NFT collection and mint NFT of the CV
+
+    return res.send('CV created')
+});
+
+// add metadata to the NFT of the CV
+router.get('/user/addJob', (req, res) => {
+    console.log("adding new job")
+    // add metadata to the NFT
+
+    return res.send('Job added')
+});
+
+module.exports = router;
